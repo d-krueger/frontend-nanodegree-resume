de3e734e29da3c0d426de5e6a0e3ea32f8b66ea5
David Krüger <david.krueger123@gmail.com>
2015-02-22 21:50:34 +0100

    refactor code use variables for constants

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,9 @@
 <!-- Added script for appending funThoughts -->
+var name = "David";
+var role = "Founder";
 
-var formattedName = HTMLheaderName.replace("%data%", "David");
-var formattedRole = HTMLheaderRole.replace("%data%", "Founder");
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
