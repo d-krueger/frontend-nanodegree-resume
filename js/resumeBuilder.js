ba3c4680b2dc19cad11d9d816298d56cc35385ce
David Krüger <david.krueger123@gmail.com>
2015-02-22 21:38:33 +0100

    Replace fun segment from js code from index to resumebuilder

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+<!-- Added script for appending funThoughts -->
+
+var awesomeThoughts = "I am David and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(awesomeThoughts);
+$("#main").append(funThoughts);
