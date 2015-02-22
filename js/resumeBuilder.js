0c00a7863bc297273903f3de671a015fa76b3084
David Krüger <david.krueger123@gmail.com>
2015-02-22 21:47:31 +0100

    Add formatted header - delete Fun/Awesome printout

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,7 @@
 <!-- Added script for appending funThoughts -->
 
-var awesomeThoughts = "I am David and I am AWESOME!";
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-console.log(awesomeThoughts);
-$("#main").append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", "David");
+var formattedRole = HTMLheaderRole.replace("%data%", "Founder");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
