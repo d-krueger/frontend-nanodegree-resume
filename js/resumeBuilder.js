5cba32f9734239555baf1b53d9b86d169325184b
David Krüger <david.krueger123@gmail.com>
2015-02-23 09:19:27 +0100

    Add Object bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,25 @@
 <!-- Added script for appending funThoughts -->
 var name = "David";
 var role = "Founder";
+var bio = {
+  "name": "David",
+  "role": "Founder",
+  "contact": {
+    "email" : "david@whatever.com",
+    "phone" : "017534232323",
+  },
+  "picture": "http://smipeapp.de/assets/img/thumb_up.png",
+  "welcome": "Hello I'm David - thanks for looking at my profile!",
+  "skills" : ["Everything", "Innovation"],
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+};
+
+
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#main").append(bio.contact.email + bio.contact.phone + bio.picture + bio.welcome + bio.skills[0]);
